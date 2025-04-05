c05a3e6463ae947711d3b1a7dfd09351627734d8
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-04-06 02:56:37 +0800

    Add render pass abstraction

---
+++ PaperEngine/src/Platform/Vulkan/VulkanTexture2D.cpp
@@ -0,0 +1,243 @@
+﻿
+#include <PaperEngine/core/Logger.h>
+
+#include <Platform/Vulkan/VulkanContext.h>
+#include <Platform/Vulkan/VulkanUtils.h>
+
+#include "VulkanTexture2D.h"
+
+namespace PaperEngine {
+
+	VulkanTexture2D::VulkanTexture2D(uint32_t width, uint32_t height, VkFormat format, VkImageUsageFlags usage) :
+		m_width(width), m_height(height), m_format(format)
+	{
+		VkImageCreateInfo image_info = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+			.pNext = nullptr,
+			.flags = 0,
+			.imageType = VK_IMAGE_TYPE_2D,
+			.format = format,		// TODO: make this configurable
+			.extent = { width, height, 1 },			// TODO: make this configurable
+			.mipLevels = 1,
+			.arrayLayers = 1,
+			.samples = VK_SAMPLE_COUNT_1_BIT,		// TODO: make this configurable
+			.tiling = VK_IMAGE_TILING_OPTIMAL,
+			.usage = usage,	// TODO: make this configurable
+			.sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+			.queueFamilyIndexCount = 0,
+			.pQueueFamilyIndices = nullptr,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+		};
+
+		VmaAllocationCreateInfo alloc_info = {
+            .usage = VmaMemoryUsage::VMA_MEMORY_USAGE_GPU_ONLY
+		};
+
+		CHECK_VK_RESULT(vmaCreateImage(VulkanContext::GetAllocator(), &image_info, &alloc_info, &m_image, &m_allocation, nullptr));
+		PE_CORE_TRACE("[VulkanTexture2D] image created. {}", (void*)m_image);
+
+		VkImageViewCreateInfo view_info = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO,
+			.pNext = nullptr,
+			.flags = 0,
+			.image = m_image,
+			.viewType = VK_IMAGE_VIEW_TYPE_2D,
+			.format = format,
+			.components = {
+				.r = VK_COMPONENT_SWIZZLE_IDENTITY,
+				.g = VK_COMPONENT_SWIZZLE_IDENTITY,
+				.b = VK_COMPONENT_SWIZZLE_IDENTITY,
+				.a = VK_COMPONENT_SWIZZLE_IDENTITY
+			},
+			.subresourceRange = {
+				.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+				.baseMipLevel = 0,
+				.levelCount = 1,
+				.baseArrayLayer = 0,
+				.layerCount = 1
+			}
+		};
+		if (format == VulkanContext::GetPhysicalDeviceInfo().depth_format) {
+			view_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+		}
+
+		CHECK_VK_RESULT(vkCreateImageView(VulkanContext::GetDevice(), &view_info, nullptr, &m_imageView));
+		PE_CORE_TRACE("[VulkanTexture2D] image view created. {}", (void*)m_imageView);
+
+	}
+
+	VulkanTexture2D::~VulkanTexture2D()
+	{
+		if (m_image) {
+			vkDestroyImageView(VulkanContext::GetDevice(), m_imageView, nullptr);
+			PE_CORE_TRACE("[VulkanTexture2D] image view destroyed. {}", (void*)m_imageView);
+			m_imageView = VK_NULL_HANDLE;
+			vmaDestroyImage(VulkanContext::GetAllocator(), m_image, m_allocation);
+			PE_CORE_TRACE("[VulkanTexture2D] image destroyed. {}", (void*)m_image);
+			m_image = VK_NULL_HANDLE;
+			m_allocation = VK_NULL_HANDLE;
+		}
+	}
+
+	void VulkanTexture2D::transitLayout(VkCommandBuffer cmdBuffer, VkImageLayout newLayout, VkImageLayout oldLayout)
+	{
+		if (oldLayout == VK_IMAGE_LAYOUT_MAX_ENUM) {
+			oldLayout = m_currentLayout;
+		}
+
+		VkImageMemoryBarrier barrier = {
+			.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+			.oldLayout = oldLayout,
+			.newLayout = newLayout,
+			.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+			.image = m_image,
+			.subresourceRange = {
+				.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+				.baseMipLevel = 0,
+				.levelCount = 1,
+				.baseArrayLayer = 0,
+				.layerCount = 1
+			}
+		};
+
+		VkPipelineStageFlags sourceStage = VK_PIPELINE_STAGE_NONE;
+		VkPipelineStageFlags destinationStage = VK_PIPELINE_STAGE_NONE;
+
+		if (newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL ||
+			(m_format == VK_FORMAT_D16_UNORM) ||
+			(m_format == VK_FORMAT_X8_D24_UNORM_PACK32) ||
+			(m_format == VK_FORMAT_D32_SFLOAT) ||
+			(m_format == VK_FORMAT_S8_UINT) ||
+			(m_format == VK_FORMAT_D16_UNORM_S8_UINT) ||
+			(m_format == VK_FORMAT_D24_UNORM_S8_UINT))
+		{
+			barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+			auto hasStencilComponent = [](VkFormat format) {
+				return (format == VK_FORMAT_D24_UNORM_S8_UINT) || (format == VK_FORMAT_D32_SFLOAT_S8_UINT);
+				};
+			if (hasStencilComponent(m_format)) {
+				barrier.subresourceRange.aspectMask |= VK_IMAGE_ASPECT_STENCIL_BIT;
+			}
+		}
+		else {
+			barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		}
+
+		if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+			barrier.srcAccessMask = 0;
+			barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+			destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		}
+		else if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_GENERAL) {
+			barrier.srcAccessMask = 0;
+			barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+			destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		}
+
+		if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED &&
+			newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+			barrier.srcAccessMask = 0;
+			barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+			destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+		} /* Convert back from read-only to updateable */
+		else if (oldLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL) {
+			barrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+			barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+			destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+		} /* Convert from updateable texture to shader read-only */
+		else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL &&
+			newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+			barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+			barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+			destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		} /* Convert depth texture from undefined state to depth-stencil buffer */
+		else if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+			barrier.srcAccessMask = 0;
+			barrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+			destinationStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+		} /* Wait for render pass to complete */
+		else if (oldLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+			barrier.srcAccessMask = 0; // VK_ACCESS_SHADER_READ_BIT;
+			barrier.dstAccessMask = 0;
+			/*
+					sourceStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+			///		destinationStage = VK_PIPELINE_STAGE_ALL_GRAPHICS_BIT;
+					destinationStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+			*/
+			sourceStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+			destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		} /* Convert back from read-only to color attachment */
+		else if (oldLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL) {
+			barrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+			barrier.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+			destinationStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+		} /* Convert from updateable texture to shader read-only */
+		else if (oldLayout == VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+			barrier.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+			barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+			destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		} /* Convert back from read-only to depth attachment */
+		else if (oldLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+			barrier.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+			barrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+			destinationStage = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+		} /* Convert from updateable depth texture to shader read-only */
+		else if (oldLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+			barrier.srcAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+			barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+			sourceStage = VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+			destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+		}
+
+		vkCmdPipelineBarrier(
+			cmdBuffer,
+			sourceStage,
+			destinationStage,
+			0,
+			0, nullptr,
+			0, nullptr,
+			1, &barrier
+		);
+		m_currentLayout = newLayout;
+	}
+
+	void VulkanTexture2D::record_copy_in_gpu(VkCommandBuffer cmd, VkBuffer srcBuffer, VkDeviceSize src_offset)
+	{
+		VkBufferImageCopy region = {
+			.bufferOffset = src_offset,
+			.bufferRowLength = 0,
+			.bufferImageHeight = 0,
+			.imageSubresource = {
+				.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+				.mipLevel = 0,
+				.baseArrayLayer = 0,
+				.layerCount = 1
+			},
+			.imageOffset = { 0, 0, 0 },
+			.imageExtent = { m_width, m_height, 1 }
+		};
+		vkCmdCopyBufferToImage(cmd, srcBuffer, m_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
+	}
+
+
+}
---
+++ PaperEngine/src/Platform/Vulkan/VulkanTexture2D.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <vulkan/vulkan.h>
+#include <vk_mem_alloc.h>
+
+namespace PaperEngine {
+
+	/// <summary>
+	/// No material properties
+	/// </summary>
+	class VulkanTexture2D {
+	public:
+		VulkanTexture2D(uint32_t width, uint32_t height, VkFormat format, VkImageUsageFlags usage);
+
+		~VulkanTexture2D();
+
+		void transitLayout(VkCommandBuffer cmdBuffer, VkImageLayout newLayout, VkImageLayout oldLayout = VK_IMAGE_LAYOUT_MAX_ENUM);
+
+		void record_copy_in_gpu(VkCommandBuffer cmd, VkBuffer srcBuffer, VkDeviceSize src_offset = 0);
+
+		VkImage get_image() const { return m_image; }
+
+		VkImageView get_image_view() const { return m_imageView; }
+
+	private:
+
+		uint32_t m_width, m_height;
+
+		VkFormat m_format{ VK_FORMAT_UNDEFINED };
+
+		// not correct at runtime
+		VkImageLayout m_currentLayout{ VK_IMAGE_LAYOUT_UNDEFINED };
+
+		VkImage m_image{ VK_NULL_HANDLE };
+		VkImageView m_imageView{ VK_NULL_HANDLE };			// default 2d view for this image
+		VmaAllocation m_allocation{ VK_NULL_HANDLE };
+	};
+
+}
