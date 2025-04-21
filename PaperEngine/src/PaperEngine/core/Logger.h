552fd3cd5f4f4b6a127062ce9f134573d716be92
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-04-21 20:44:01 +0800

    Update Logger.h

--- PaperEngine/src/PaperEngine/core/Logger.h
+++ PaperEngine/src/PaperEngine/core/Logger.h
@@ -2,14 +2,18 @@
 
 #include "Base.h"
 
+#pragma warning(push)
+#pragma warning(disable : 6294)
+#pragma warning(disable : 26495)
+#pragma warning(disable : 26498)
 #include <spdlog/spdlog.h>
-
+#pragma warning(pop)
 namespace PaperEngine {
 
 	class Logger {
 	public:
 		static void Init();
-		inline static Ref<spdlog::logger>& getCoreLogger() { return s_coreLogger; }
+		inline static Ref<spdlog::logger>& GetCoreLogger() { return s_coreLogger; }
 	private:
 		static Ref<spdlog::logger> s_coreLogger;
 	};
@@ -17,12 +21,12 @@ namespace PaperEngine {
 }
 
 #ifdef PE_DEBUG
-#define PE_CORE_TRACE(...)    ::PaperEngine::Logger::getCoreLogger()->trace(__VA_ARGS__)
+#define PE_CORE_TRACE(...)    ::PaperEngine::Logger::GetCoreLogger()->trace(__VA_ARGS__)
 #else
 #define PE_CORE_TRACE(...)
 #endif
-#define PE_CORE_INFO(...)     ::PaperEngine::Logger::getCoreLogger()->info(__VA_ARGS__)
-#define PE_CORE_WARN(...)     ::PaperEngine::Logger::getCoreLogger()->warn(__VA_ARGS__)
-#define PE_CORE_ERROR(...)    ::PaperEngine::Logger::getCoreLogger()->error(__VA_ARGS__)
-#define PE_CORE_CRITICAL(...) ::PaperEngine::Logger::getCoreLogger()->critical(__VA_ARGS__)
+#define PE_CORE_INFO(...)     ::PaperEngine::Logger::GetCoreLogger()->info(__VA_ARGS__)
+#define PE_CORE_WARN(...)     ::PaperEngine::Logger::GetCoreLogger()->warn(__VA_ARGS__)
+#define PE_CORE_ERROR(...)    ::PaperEngine::Logger::GetCoreLogger()->error(__VA_ARGS__)
+#define PE_CORE_CRITICAL(...) ::PaperEngine::Logger::GetCoreLogger()->critical(__VA_ARGS__)
 
