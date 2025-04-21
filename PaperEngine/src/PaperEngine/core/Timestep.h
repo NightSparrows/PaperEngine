a86d0c2700c781cb9fc745e5a0c4b04b4b4aeeb9
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-04-21 20:44:53 +0800

    Create Timestep.h

---
+++ PaperEngine/src/PaperEngine/core/Timestep.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+
+namespace PaperEngine {
+
+	class Timestep
+	{
+	public:
+
+		template<typename Rep, typename Period>
+		Timestep(std::chrono::duration<Rep, Period> duration)
+			: m_time(std::chrono::duration_cast<std::chrono::nanoseconds>(duration))
+		{
+		}
+
+	private:
+		std::chrono::nanoseconds m_time;
+	};
+
+}
