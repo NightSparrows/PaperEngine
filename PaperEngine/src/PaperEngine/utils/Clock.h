b629807a4bef5d135f25e2e34432a3d5f4b5753e
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-04-21 20:45:40 +0800

    Create Clock.h

---
+++ PaperEngine/src/PaperEngine/utils/Clock.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <chrono>
+
+#include <PaperEngine/core/Timestep.h>
+
+namespace PaperEngine {
+
+	class Clock {
+	public:
+
+		Timestep reset() {
+			auto current_time = std::chrono::high_resolution_clock::now();
+			auto delta_time = current_time - m_startTime;
+			return Timestep(delta_time);
+		}
+
+		Timestep get_delta_time() const {
+			auto current_time = std::chrono::high_resolution_clock::now();
+			return Timestep(current_time - m_startTime);
+		}
+
+	private:
+
+		std::chrono::steady_clock::time_point m_startTime;
+	};
+
+}
