de9ec432ab94bd44e897f92fea565fb4f339a744
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-04-26 19:15:35 +0800

    Add keyboard isKeyDown

---
+++ PaperEngine/src/PaperEngine/core/Keyboard.cpp
@@ -0,0 +1,16 @@
+﻿#include "Keyboard.h"
+
+#include "Application.h"
+#include "Window.h"
+#include "Platform/Windows/core/WindowsWindow.h"
+
+namespace PaperEngine {
+
+	bool Keyboard::IsKeyDown(KeyCode keyCode)
+	{
+		GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().get_window().get_native_window());
+
+		return glfwGetKey(window, keyCode) == GLFW_PRESS;
+	}
+
+}
--- PaperEngine/src/PaperEngine/core/Keyboard.h
+++ PaperEngine/src/PaperEngine/core/Keyboard.h
@@ -1,2 +1,15 @@
 ﻿#pragma once
 
+#include "Base.h"
+#include "KeyCodes.h"
+
+namespace PaperEngine {
+
+	class Keyboard {
+	public:
+
+		PE_API static bool IsKeyDown(KeyCode keyCode);
+
+	};
+
+}
