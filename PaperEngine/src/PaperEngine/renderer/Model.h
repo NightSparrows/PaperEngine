c1fa7e43ec0d5fceeacf93c20b9878a2bf0c7508
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-05-20 01:02:35 +0800

    fix debug build error

---
+++ PaperEngine/src/PaperEngine/renderer/Model.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Mesh.h"
+#include "Material.h"
+
+namespace PaperEngine {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	struct Model {
+		MeshHandle mesh;
+		std::vector<MaterialHandle> materials;
+	};
+
+}
