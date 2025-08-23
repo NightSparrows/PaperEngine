82191e47c9c42effb538a37588a8d0ba4c86911d
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-08-23 19:09:33 +0800

    Add basic transform and its component for my engine

---
+++ PaperEngine/src/PaperEngine/utils/Transform.cpp
@@ -0,0 +1,76 @@
+﻿#include "Transform.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace PaperEngine {
+    PE_API void Transform::rotate(const glm::vec3& axis, float angle)
+    {
+        m_isDirty = true;
+        this->m_rotation = glm::rotate(m_rotation, glm::radians(angle), axis);
+    }
+
+    const Transform& Transform::operator=(const glm::mat4& matrix)
+    {
+        glm::vec3 position;
+        glm::quat rotation;
+        glm::vec3 scale;
+        glm::vec3 skew;
+        glm::vec4 perspective;
+        bool success = glm::decompose(matrix, scale, rotation, position, skew, perspective);
+        if (success) {
+            m_isDirty = true;
+            m_position = position;
+            m_rotation = rotation;
+            m_scale = scale;
+        }
+
+        return *this;
+    }
+
+    PE_API glm::vec3 Transform::get_forward() const
+    {
+        update();
+        return m_rotation * glm::vec3(0, 0, -1);
+    }
+
+    PE_API glm::vec3 Transform::get_right() const
+    {
+        update();
+        return m_rotation * glm::vec3(1, 0, 0);
+    }
+
+    PE_API glm::vec3 Transform::get_up() const
+    {
+        update();
+        return m_rotation * glm::vec3(0, 1, 0);
+    }
+
+    PE_API const glm::mat4& Transform::matrix() const
+    {
+        update();
+        return m_matrix;
+    }
+    PE_API Transform::operator const glm::mat4& () const
+    {
+        update();
+        return m_matrix;
+    }
+
+    void Transform::update() const
+    {
+        if (!m_isDirty)
+            return;
+
+
+        glm::mat4 posMatrix = glm::translate(glm::mat4(1.f), this->m_position);
+        glm::mat4 rotMatrix = glm::toMat4(this->m_rotation);
+        glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.f), this->m_scale);
+        this->m_matrix = posMatrix * rotMatrix * scaleMatrix;
+        this->m_isDirty = false;
+    }
+
+}
---
+++ PaperEngine/src/PaperEngine/utils/Transform.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+#include <PaperEngine/core/Base.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+namespace PaperEngine {
+
+	class Transform {
+	public:
+
+		Transform() = default;
+		Transform(const Transform&) = default;
+		Transform& operator=(const Transform&) = default;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="axis"></param>
+		/// <param name="angle">
+		/// in degree
+		/// </param>
+		/// <returns></returns>
+		PE_API void rotate(const glm::vec3& axis, float angle);
+
+		PE_API const Transform& operator=(const glm::mat4& matrix);
+
+		PE_API void set_position(const glm::vec3& position) { m_isDirty = true; m_position = position; }
+
+		PE_API void set_rotation(const glm::quat& rotation) { m_isDirty = true; m_rotation = rotation; }
+
+		PE_API void set_scale(const glm::vec3& scale) { m_isDirty = true; m_scale = scale; }
+
+		PE_API glm::vec3& get_position() { m_isDirty = true; return m_position; }
+
+		PE_API const glm::vec3& get_position() const { return m_position; }
+
+		PE_API const glm::quat& get_rotation() const { return m_rotation; }
+
+		PE_API glm::quat& get_rotation() { return m_rotation; }
+
+		PE_API const glm::vec3& get_scale() const { return m_scale; }
+
+		PE_API glm::vec3& get_scale() { return m_scale; }
+
+		PE_API glm::vec3 get_forward() const;
+
+		PE_API glm::vec3 get_right() const;
+
+		PE_API glm::vec3 get_up() const;
+
+		PE_API const glm::mat4& matrix() const;
+
+		PE_API operator const glm::mat4& () const;
+
+	protected:
+		void update() const;
+
+	protected:
+		glm::vec3 m_position{ 0, 0, 0 };
+		glm::quat m_rotation = glm::identity<glm::quat>();
+		glm::vec3 m_scale{ 1.f, 1.f, 1.f };
+
+		mutable bool m_isDirty{ true };
+		mutable glm::mat4 m_matrix = glm::identity<glm::mat4>();
+
+	};
+
+}
+
