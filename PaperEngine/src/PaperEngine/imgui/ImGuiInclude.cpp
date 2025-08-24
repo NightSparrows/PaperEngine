8e2f32ef4aa5e0d2393c6f02e6bdc7850c3cf81d
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-08-24 19:39:34 +0800

    move submodules from fetch content to git submodule

--- PaperEngine/src/PaperEngine/imgui/ImGuiInclude.cpp
+++ PaperEngine/src/PaperEngine/imgui/ImGuiInclude.cpp
@@ -3,9 +3,3 @@
 
 // TODO 如果不用GLFW記得
 #include "backends/imgui_impl_glfw.cpp"
-
-#include "imgui.cpp"
-#include "imgui_draw.cpp"
-#include "imgui_widgets.cpp"
-#include "imgui_tables.cpp"
-#include "imgui_demo.cpp"
