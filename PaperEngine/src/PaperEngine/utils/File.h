2d6f3279370cbfdfc80ef81c391967aeb7786340
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2025-08-26 16:48:41 +0800

    add very basic file class

---
+++ PaperEngine/src/PaperEngine/utils/File.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <filesystem>
+#include <fstream>
+
+#include <PaperEngine/core/Base.h>
+
+namespace PaperEngine {
+
+	/// <summary>
+	/// 簡單的檔案IO class
+	/// </summary>
+	class File {
+	public:
+		PE_API File() = default;
+		PE_API File(const std::filesystem::path& filePath);
+		
+		PE_API void* readFully(size_t& size);
+
+		PE_API void setFilePath(const std::filesystem::path& filePath);
+
+		inline PE_API const std::filesystem::path& getFilePath() const { return m_path; }
+
+	private:
+		std::filesystem::path m_path;
+	};
+
+}
